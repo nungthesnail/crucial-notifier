cf409ae7e447a18e8c5904e0391069be9560b276
Alexander Zakharchuk <nungthesnail@mail.ru>
2025-04-01 18:14:29 +0500

    Crucial.EntityFrameworkCore: Модуль доступа к данным через EF Core

---
+++ Crucial.EntityFrameworkCore/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Crucial.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<CourseModel> Courses { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ConfigureCourses(modelBuilder.Entity<CourseModel>());
+    }
+
+    private static void ConfigureCourses(EntityTypeBuilder<CourseModel> builder)
+    {
+        builder.ToTable("course");
+        builder.HasKey(static x => x.Id);
+        
+        builder
+            .HasIndex(static x => x.Name)
+            .IsUnique();
+        
+        builder
+            .Property(static x => x.Id)
+            .HasField("id")
+            .IsRequired();
+        builder
+            .Property(static x => x.Name)
+            .HasField("name")
+            .HasMaxLength(128)
+            .IsRequired();
+        builder
+            .Property(static x => x.Description)
+            .HasField("description")
+            .HasMaxLength(512);
+        builder
+            .Property(static x => x.TotalLessonsCount)
+            .HasField("total_lessons_count")
+            .IsRequired();
+        builder
+            .Property(static x => x.LessonsPassedCount)
+            .HasField("lessons_passed_count")
+            .HasDefaultValue(0)
+            .IsRequired();
+    }
+}
---
+++ Crucial.EntityFrameworkCore/CourseModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crucial.EntityFrameworkCore;
+
+public class CourseModel
+{
+    public Guid Id { get; set; }
+    [MaxLength(128)]
+    public required string Name { get; set; }
+    [MaxLength(512)]
+    public string? Description { get; set; }
+    public int TotalLessonsCount { get; set; }
+    public int LessonsPassedCount { get; set; }
+}
